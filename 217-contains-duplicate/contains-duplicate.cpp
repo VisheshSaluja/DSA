236e69b37fca858144ed84b04acc2cd7827b2e83
Vishesh Saluja <70054173+VisheshSaluja@users.noreply.github.com>
2025-02-12 18:53:29 -0500

    Time: 36 ms (37.34%) | Memory: 75.8 MB (12.12%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,10 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> s;
 
-        for(int i=0; i<nums.size(); i++){
-            s.insert(nums[i]);
-        }
+      for(int i=0;i<nums.size();i++){
+        s.insert(nums[i]);
+      }
+      int n = s.size();
 
-        return !(nums.size() == s.size());
+      return !(n==nums.size());
     }
 };
