a47a3a05c5c40521223b93b8766af51f9c878218
Vishesh Saluja <70054173+VisheshSaluja@users.noreply.github.com>
2025-07-09 21:39:23 -0400

    Time: 3 ms (41.21%) | Memory: 14.3 MB (25.48%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    
+    set<vector<int>> ans;
+    void helper(vector<int>& nums, int target, int i, set<vector<int>> &ans, vector<int> &v){
+
+        if(target==0){
+            ans.insert(v);
+            return;
+        }
+
+        if(i>=nums.size() || target<0) return;
+
+        for(int j=i;j<nums.size(); j++){
+            if(j>i && nums[j]==nums[j-1]){
+                continue;
+            }
+            // if(nums[j]>target) break;
+            v.push_back(nums[j]);
+            helper(nums, target-nums[j], j+1, ans, v);
+            v.pop_back();
+        }
+
+        // v.push_back(nums[i]);
+        // helper(nums, target-nums[i], i+1, ans, v);
+        // v.pop_back();
+        // helper(nums, target, i+1, ans, v);
+
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> v;
+        helper(candidates, target, 0, ans, v);
+        vector<vector<int>> result;
+        
+
+        for(auto it: ans){
+            result.push_back(it);
+        }
+
+        return result;
+        
+    }
+};
