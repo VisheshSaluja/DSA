3750669710142b8182af3370cb501493af1c6188
Vishesh Saluja <70054173+VisheshSaluja@users.noreply.github.com>
2025-07-09 21:43:34 -0400

    Time: 1 ms (53.15%) | Memory: 14.2 MB (30.49%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -16,16 +16,10 @@ public:
             if(j>i && nums[j]==nums[j-1]){
                 continue;
             }
-            // if(nums[j]>target) break;
             v.push_back(nums[j]);
             helper(nums, target-nums[j], j+1, ans, v);
             v.pop_back();
         }
-
-        // v.push_back(nums[i]);
-        // helper(nums, target-nums[i], i+1, ans, v);
-        // v.pop_back();
-        // helper(nums, target, i+1, ans, v);
 
     }
 
@@ -33,14 +27,9 @@ public:
         sort(candidates.begin(), candidates.end());
         vector<int> v;
         helper(candidates, target, 0, ans, v);
-        vector<vector<int>> result;
-        
+      
 
-        for(auto it: ans){
-            result.push_back(it);
-        }
-
-        return result;
+        return ans;
         
     }
 };
